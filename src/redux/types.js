e08a337b5351ac960f106162ce7e0998d396963b
Niv Koren <Niv_Koren@amat.com>
2020-09-16 00:36:48 +0300

    all redux actions

--- src/redux/types.js
+++ src/redux/types.js
@@ -1,6 +1,26 @@
-// START Counter
-export const ADD_ONE = "ADD_ONE";
-export const SUBTRACT_ONE = "SUBTRACT_ONE";
-export const ERROR = "ERROR";
+// Scenarios:
+export const CREATE_SCENARIO = "CREATE_SCENARIO";
+export const GET_SCENARIOS_LIST = "GET_SCENARIOS_LIST";
+export const GET_SCENARIO = "GET_SCENARIO";
+export const EDIT_SCENARIO = "EDIT_SCENARIO";
+export const DELETE_SCENARIO = "DELETE_SCENARIO";
 
-// END Counter
+// Nodes:
+export const CREATE_NODE = "CREATE_NODE";
+export const GET_NODE = "GET_NODE";
+export const UPDATE_NODE = "UPDATE_NODE";
+export const DELETE_NODE = "DELETE_NODE";
+
+// Options:
+export const CREATE_OPTION = "CREATE_OPTION";
+export const UPDATE_OPTION = "UPDATE_OPTION";
+export const DELETE_OPTION = "DELETE_OPTION";
+
+// Tags:
+export const GET_TAGS_LIST = "GET_TAGS_LIST";
+
+// Categories:
+export const GET_CATEGORIES_LIST = "GET_CATEGORIES_LIST";
+
+// Error:
+export const ERROR = "ERROR";

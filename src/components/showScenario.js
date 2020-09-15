b0d6a92aabedb07c3c9fe379565901c47572075f
Evgeny Lahav <Evgeny_Lahav@amat.com>
2020-09-15 14:00:12 +0300

    added stepper menu

--- src/components/showScenario.js
+++ src/components/showScenario.js
@@ -2,8 +2,31 @@ import React, { Component } from "react";
 import { addOne, subtractOne } from "../redux/actions/counterAction.js";
 import { connect } from "react-redux";
 import { scenario1, nodes } from "./constants";
+import VerticalLinearStepper from "./stepperMenu";
 
 // import "./Main.scss";
+
+function getSteps() {
+    return ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+  }
+  
+  function getStepContent(step) {
+    switch (step) {
+      case 0:
+        return `For each ad campaign that you create, you can control how much
+                you're willing to spend on clicks and conversions, which networks
+                and geographical locations you want your ads to show on, and more.`;
+      case 1:
+        return 'An ad group contains one or more ads which target a shared set of keywords.';
+      case 2:
+        return `Try out different ad text to see what brings in the most customers,
+                and learn how to enhance your ads using features like ad extensions.
+                If you run into any problems with your ads, find out how to tell if
+                they're running and how to resolve approval issues.`;
+      default:
+        return 'Unknown step';
+    }
+  }
 
 class ShowScenario extends Component {
 
@@ -27,6 +50,7 @@ class ShowScenario extends Component {
                         )
 
                     } )}
+                    <VerticalLinearStepper getSteps={getSteps} getStepContent={(id) => getStepContent(id)} />
                 </div>
             </>
         );

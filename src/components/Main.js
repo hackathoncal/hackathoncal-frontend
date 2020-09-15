5fef83883da9f50d5eb43f292af53dc8063badaf
Israel Heiblum <Israel_Heiblum@amat.com>
2020-09-15 22:39:57 +0300

    homepage version 0.1

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,12 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import ShowScenario from "./showScenario";
+import React, {Component} from "react";
+import {connect} from "react-redux";
 import "./Main.scss";
-import AddNewScenario from "./AddNewScenario";
-import Option from "./Option";
+// import AddNewScenario from "./AddNewScenario";
 import AppBar from "../components/app-bar/AppBar";
 import Search from "../components/search/Search";
-import ScenarioList from "../components/scenarios-list/ScenarioList";
-import Dropdown from "../components/drop-downs/Dropdown";
+import ScenariosContainer from "./scenarios/ScenariosContainer";
+import DropDownListContainer from "./drop-downs/DropDownListContainer";
+
 import Container from '@material-ui/core/Container';
 
 
@@ -17,25 +16,20 @@ class Main extends Component {
         return (
             <React.Fragment>
                 <Container fixed>
-
-                <AppBar />
-                <Search />
-                <Dropdown />
-                <ScenarioList />
-                {/*<ShowScenario />*/}
+                    <AppBar/>
+                    <Search/>
+                    <DropDownListContainer/>
+                    <ScenariosContainer/>
                 </Container>
             </React.Fragment>
         );
     }
 }
 
-const mapStateToProps = (state) => ({
-});
+const mapStateToProps = (state) => ({});
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-
-    };
+    return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
